7aed319690aeb5cb8d39c4758e9ca0343ec574cc
ThomasPham <thang.pham@techvsi.com>
2023-08-28 15:04:03 +0700

    update ui

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button style={{backgroundColor:'red'}}>Change to blue</button>
+      <ul>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+      </ul>
+      <span>test</span>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import { render, screen } from '@testing-library/react';
+import { logRoles } from '@testing-library/dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  console.log(linkElement)
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial text', () => {
+  const {container}=render(<App />)
+  logRoles(container)
+  // find an element with a role of button and text of 'Change to blue'
+  const button= screen.getByRole('button', { name: /change to blue/i })
+  // expect the background color to be red
+  expect(button).toHaveStyle({ backgroundColor: 'red' })
 });
+
+test('button has correct initial background color', () => {
+  render(<App />)
+
+})
+
+test('button turns blue when clicked', () => {
+  render(<App />)
+
+})
+
+test('button turns red when clicked twice', () => {
+  render(<App />)
+
+})
+
